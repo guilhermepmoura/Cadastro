28129506132f25b4d14288e9ddca064c0c455af5
guilhermepmoura <guilhermepinheiromoura1@gmail.com>
2025-06-17 13:00:07 -0300

    Adição do lombok

--- src/main/java/com/exemplo/cadastro/Missoes/MissoesModel.java
+++ src/main/java/com/exemplo/cadastro/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package com.exemplo.cadastro.Missoes;
 
 import com.exemplo.cadastro.Usuarios.UsuarioModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name= "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id
@@ -19,7 +25,4 @@ public class MissoesModel {
 @OneToMany(mappedBy = "missoes") // Uma missão pode ter vários usuários.
     private List<UsuarioModel> usuario;
 
-
-    public MissoesModel() {
-    }
 }
--- src/main/java/com/exemplo/cadastro/Usuarios/UsuarioModel.java
+++ src/main/java/com/exemplo/cadastro/Usuarios/UsuarioModel.java
@@ -2,11 +2,17 @@ package com.exemplo.cadastro.Usuarios;
 
 import com.exemplo.cadastro.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name="tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class UsuarioModel {
 
     @Id
@@ -21,36 +27,4 @@ public class UsuarioModel {
     private MissoesModel missoes;
 
 
-    public UsuarioModel() {
-    }
-
-    public UsuarioModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
